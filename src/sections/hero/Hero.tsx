a22233d090318afef96c02d9f24ab5eb9a33f19d
caarlosdamian <dapecarlos@gmail.com>
2022-11-21 17:44:00 -0600

    feat(hero): added hero section

--- src/sections/hero/Hero.tsx
+++ src/sections/hero/Hero.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
-import './Hero.scss'
+import React from "react";
+import HeroImgMobile from '../../assets/images/image-web-3-mobile.jpg'
+import "./Hero.scss";
 
 export const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section id='#hero' className="hero-container">
+      <div className="hero-img"></div>
+      <div className="hero-section">
+        <span className="hero-header">The Bright Future of Web 3.0?</span>
+        <span className="hero-description">We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?</span>
+        <button className="hero-button">READ MORE</button>
+      </div>
+    </section>
+  );
+};
